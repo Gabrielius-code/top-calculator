2681b65de938b2c0ef052901face77fe8f54620b
Gabrielius-code <gabrieliusbutkus2005@gmail.com>
2022-07-26 19:37:54 +0300

    Added event listeners to numbers

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 class Calculator {
-  constructor() {}
-  add(a, b) {
+  #numbers = document.querySelectorAll("[data-number]");
+  constructor() {
+    this.addEventListenersToNum();
+  }
+  addEventListenersToNum() {
+    this.#numbers.forEach((number) =>
+      number.addEventListener("click", this.handleNumClick.bind(this))
+    );
+  }
+  handleNumClick(e) {
+    //FOR NOW
+    console.log(e.target.dataset.number);
+  }
+  #add(a, b) {
     return a + b;
   }
-  subtract(a, b) {
+  #subtract(a, b) {
     return a - b;
   }
-  multiply(a, b) {
+  #multiply(a, b) {
     return a * b;
   }
-  divide(a, b) {
+  #divide(a, b) {
     return a / b;
   }
+  #operate(operator, num1, num2) {}
 }
 const calculator = new Calculator();
