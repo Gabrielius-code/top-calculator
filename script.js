a3e3911a2e2e7a16e090866ef508367c934d9277
Gabrielius-code <gabrieliusbutkus2005@gmail.com>
2022-07-27 19:05:41 +0300

    Basic working calculator

--- script.js
+++ script.js
@@ -1,16 +1,54 @@
 class Calculator {
   #numbers = document.querySelectorAll("[data-number]");
+  #operators = document.querySelectorAll("[data-operator]");
+  #firstNum = "";
+  #secondNum = "";
+  #answer = "";
+  #input = document.querySelector(".input");
+  #previuosInput = document.querySelector(".previous-input");
+  #operator = "";
+  #equalBtn = document.querySelector(".equal");
   constructor() {
-    this.addEventListenersToNum();
+    this.#addEventListenersToBtns();
   }
-  addEventListenersToNum() {
+  #addEventListenersToBtns() {
     this.#numbers.forEach((number) =>
-      number.addEventListener("click", this.handleNumClick.bind(this))
+      number.addEventListener("click", this.#handleNumClick.bind(this))
     );
+    this.#operators.forEach((operator) =>
+      operator.addEventListener("click", this.#handleOperatorClick.bind(this))
+    );
+    this.#equalBtn.addEventListener("click", this.#handleEqualClick.bind(this));
   }
-  handleNumClick(e) {
+  #handleNumClick(e) {
     //FOR NOW
-    console.log(e.target.dataset.number);
+
+    if (!this.#operator) {
+      if (this.#answer || this.#answer === 0) {
+        this.#init();
+      } //If I have an answer like 100 and I press number (for example 1), then it should clear firstNum, because it shouldn't be 1001, it should be 1
+      this.#firstNum += e.target.dataset.number;
+    }
+    if (this.#operator) this.#secondNum += e.target.dataset.number;
+    this.#displayInput(e.target.dataset.number);
+    console.log(`Pirmas:${this.#firstNum} Antras:${this.#secondNum}`);
+  }
+  #handleOperatorClick(e) {
+    if (this.#operator)
+      this.#operate(this.#operator, this.#firstNum, this.#secondNum);
+    this.#operator = e.target.dataset.operator;
+    this.#displayInput(this.#operator);
+  }
+  #handleEqualClick() {
+    this.#operate(this.#operator, this.#firstNum, this.#secondNum);
+  }
+  #init() {
+    this.#firstNum = "";
+    this.#secondNum = "";
+    this.#input.textContent = "";
+    this.#previuosInput.textContent = "";
+    this.#answer = "";
+    this.#operator = "";
   }
   #add(a, b) {
     return a + b;
@@ -24,6 +62,33 @@ class Calculator {
   #divide(a, b) {
     return a / b;
   }
-  #operate(operator, num1, num2) {}
+  #operate(operator, num1, num2) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+    switch (operator) {
+      case "+":
+        this.#answer = this.#add(num1, num2);
+        break;
+      case "-":
+        this.#answer = this.#subtract(num1, num2);
+        break;
+      case "*":
+        this.#answer = this.#multiply(num1, num2);
+        break;
+      case "/":
+        this.#answer = this.#divide(num1, num2);
+        break;
+    }
+    this.#displayAnswer(this.#answer);
+    this.#firstNum = this.#answer;
+    this.#secondNum = "";
+    this.#operator = "";
+  }
+  #displayInput(input) {
+    this.#input.textContent += input;
+  }
+  #displayAnswer(answer) {
+    this.#input.textContent = answer;
+  }
 }
 const calculator = new Calculator();
